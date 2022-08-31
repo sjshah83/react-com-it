4ce29255cf0ef36fee3ae15ca0769218c61343ad
sjshah83 <sjshah83@gmail.com>
2022-08-30 19:34:44 -0600

    Added Invoice list and DefaultROute

--- Projects/router-app/src/index.js
+++ Projects/router-app/src/index.js
@@ -15,6 +15,11 @@ root.render(
         <Route path="/" element={<App />}>
           <Route path="/expenses" element={<Expenses />} />
           <Route path="/invoices" element={<Invoices />} />
+          <Route path="/*" element={
+            <main style={{ padding : "1em"}}>
+              <p>There's nothing here! </p>
+            </main>
+          } />
         </Route>
       </Routes>
     </BrowserRouter>
