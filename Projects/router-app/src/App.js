e2906f17a4deda261513a98a1ef0a14ab078cdcb
sjshah83 <sjshah83@gmail.com>
2022-08-30 18:26:39 -0600

    Added Outlet

--- Projects/router-app/src/App.js
+++ Projects/router-app/src/App.js
@@ -1,10 +1,19 @@
 
 import './App.css';
+import { Link , Outlet} from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <h1>Book-keeper</h1> 
+      <h1>Book-keeper</h1>
+      <nav style={{
+        borderBottom: "double 2px",
+        paddingBottom: "1em",
+      }}>
+        <Link to="/invoices">Invoices</Link> | {" "}
+        <Link to="/expenses">Expensis</Link>
+      </nav>
+      <Outlet/>
     </div>
   );
 }
