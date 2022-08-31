0842d4f7cec0430246f3a074ff4903a8ab9c8e04
sjshah83 <sjshah83@gmail.com>
2022-08-30 20:20:30 -0600

    Added display Invoice object

--- Projects/router-app/src/routes/invoice.jsx
+++ Projects/router-app/src/routes/invoice.jsx
@@ -1,9 +1,18 @@
 import { useParams } from "react-router-dom";
+import { getInvoice } from "../data";
 
-const Invoice= ()=> {
+const Invoice = () => {
     let params = useParams();
-    return <h2 style={{ padding : "1em" }}>
-        Invoice : {params.invoiceId}
+    let invoice = getInvoice(parseInt(params.invoiceId, 10));
+    
+    return <h2 style={{ padding: "1em" }}>
+        <main style={{ padding: "1rem" }}>
+            <h2>Total Due: {invoice.amount}</h2>
+            <p>
+                {invoice.name}: {invoice.number}
+            </p>
+            <p>Due Date: {invoice.due}</p>
+        </main>
     </h2>;
 }
 
