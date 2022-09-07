7cacddf5752972f1c04b343615dbd1b8a35379eb
sjshah83 <sjshah83@gmail.com>
2022-09-06 18:52:08 -0600

    Added search-param 'Filter'

--- Projects/router-app/src/routes/invoices.jsx
+++ Projects/router-app/src/routes/invoices.jsx
@@ -1,16 +1,33 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useSearchParams } from "react-router-dom";
 import { getInvoices } from "../data.js"
 
 export default function Invoices() {
     let invoices = getInvoices();
+    let [searchParams, setSearchParams] = useSearchParams();
     return (
         <div style={{ display: "flex" }}>
             <nav style={{
                 borderRight: "solid 1px",
                 padding: "1em",
             }}>
+                <input
+                    value={searchParams.get("filter") || ""}
+                    onChange={(event) => {
+                        let filter = event.target.value;
+                        if (filter) {
+                            setSearchParams({ filter });
+                        } else {
+                            setSearchParams({});
+                        }
+                    }}
+                />
                 {
-                    invoices.map((invoice) => (
+                    invoices.filter((invoice) => {
+                        let filter = searchParams.get("filter");
+                        if (!filter) return true;
+                        let name = invoice.name.toLowerCase();
+                        return name.startsWith(filter.toLowerCase());
+                    }).map((invoice) => (
                         <NavLink
                             style={({ isActive }) => {
                                 return {
