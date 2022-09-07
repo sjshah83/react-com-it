14f3cfb6af55fb5a86d7946931563ddd36a8e52a
sjshah83 <sjshah83@gmail.com>
2022-09-06 19:52:48 -0600

    Added Delete feature for invoice

--- Projects/router-app/src/routes/invoice.jsx
+++ Projects/router-app/src/routes/invoice.jsx
@@ -1,9 +1,11 @@
-import { useParams } from "react-router-dom";
-import { getInvoice } from "../data";
+import { useParams, useNavigate, useLocation } from "react-router-dom";
+import { deleteInvoice, getInvoice } from "../data";
 
 const Invoice = () => {
     let params = useParams();
     let invoice = getInvoice(parseInt(params.invoiceId, 10));
+    let location = useLocation();
+    let navigate = useNavigate();
 
     return (
         <main style={{ padding: "1em" }}>
@@ -12,6 +14,16 @@ const Invoice = () => {
                 {invoice.name}: {invoice.number}
             </p>
             <p>Due Date: {invoice.due}</p>
+            <p>
+                <button
+                    onClick={() => {
+                        deleteInvoice(invoice.number);
+                        navigate("/invoices" + location.search);
+                        alert("Search term :"+location.search);
+                    }}>
+                    Delete
+                </button>
+            </p>
         </main>
     );
 }
