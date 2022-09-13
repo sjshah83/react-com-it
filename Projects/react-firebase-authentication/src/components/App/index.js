183c7d4a3a92ed2f089b12c67bbe2acb151c922a
sjshah83 <sjshah83@gmail.com>
2022-09-12 20:08:55 -0600

    Moved routes to index.js and added outlet tag

--- Projects/react-firebase-authentication/src/components/App/index.js
+++ Projects/react-firebase-authentication/src/components/App/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+
+import Navigation from '../Navigation'
+
+
 const App = () => (
-  <div>
-    <h1>App</h1>
-  </div>
+    <div>
+      
+      <hr />
+      <Outlet/>
+       
+    </div>
 );
 
 export default App;
--- Projects/react-firebase-authentication/src/index.js
+++ Projects/react-firebase-authentication/src/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
-import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
+import * as ROUTES from './constants/routes'
+import LandingPage from './components/Landing';
+import SignUpPage from './components/SignUp';
+import SignInPage from './components/SignIn';
+import PasswordForgetPage from './components/PasswordForget';
+import HomePage from './components/Home';
+import AccountPage from './components/Account';
+import AdminPage from './components/Admin';
+import Navigation from './components/Navigation';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <div>
+
+        <Navigation />
+        <hr />
+      </div>
+      <Routes>
+        <Route exact path={ROUTES.LANDING} element={<LandingPage />} />
+        <Route path={ROUTES.SIGN_UP} element={<SignUpPage/>} />
+        <Route path={ROUTES.SIGN_IN} element={<SignInPage/>} />
+        <Route path={ROUTES.PASSWORD_FORGET} element={<PasswordForgetPage/>} />
+        <Route path={ROUTES.HOME} element={<HomePage />} />
+        <Route path={ROUTES.ACCOUNT} element={<AccountPage/>} />
+        <Route path={ROUTES.ADMIN} element={<AdminPage/>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
