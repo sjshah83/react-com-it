03a68e95c2be77055c9f489a5b460102a3cf07e7
sjshah83 <sjshah83@gmail.com>
2022-09-15 18:52:29 -0600

    Added firebase authentication

--- Projects/react-firebase-authentication/src/components/Firebase/firebase.js
+++ Projects/react-firebase-authentication/src/components/Firebase/firebase.js
@@ -1,4 +1,4 @@
-import app from  'firebase/compat/app'
+import app from 'firebase/compat/app'
 import 'firebase/compat/auth';
 
 const config = {
@@ -8,14 +8,28 @@ const config = {
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
-class Firebase{
-  constructor(){
+class Firebase {
+  constructor() {
     app.initializeApp(config);
 
     this.auth = app.auth();
   }
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
