23eb753958f7ef37519393787fe80b80d69d33db
sjshah83 <sjshah83@gmail.com>
2022-09-26 19:01:03 -0600

    Updates firebase.js for Realtime Database connectivity

--- Projects/react-firebase-authentication/src/components/Firebase/firebase.js
+++ Projects/react-firebase-authentication/src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/compat/app'
 import 'firebase/compat/auth';
+import 'firebase/compat/database';
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -8,7 +9,7 @@ const config = {
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 class Firebase {
@@ -16,7 +17,10 @@ class Firebase {
     app.initializeApp(config);
 
     this.auth = app.auth();
+    this.db=app.database();
   }
+
+   // *** Auth API ***
 
   doCreateUserWithEmailAndPassword = (email, password) =>
     this.auth.createUserWithEmailAndPassword(email, password);
@@ -30,6 +34,13 @@ class Firebase {
 
   doPasswordUpdate = password =>
     this.auth.currentUser.updatePassword(password);
+
+
+   // *** User API ***
+
+  user = uid => this.db.ref(`users/${uid}`);
+
+  users = () => this.db.ref('users');
 }
 
 export default Firebase;
