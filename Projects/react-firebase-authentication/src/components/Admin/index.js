edef3ddb14c522f9f17f4b50278f1760d41988ee
sjshah83 <sjshah83@gmail.com>
2022-09-26 20:31:56 -0600

    Added the admin page for user list

--- Projects/react-firebase-authentication/src/components/Admin/index.js
+++ Projects/react-firebase-authentication/src/components/Admin/index.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withFirebase } from '../Firebase';
+class AdminPage extends Component {
 
-const Admin = () => (
-  <div>
-    <h1>Admin</h1>
-  </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      users: [],
+    }
+  }
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    this.props.firebase.users().on('value', snapshot => {
+
+      const userObj = snapshot.val();
+
+      const userList = Object.keys(userObj).map(key => ({
+        ...userObj[key],
+        uid: key,
+      }));
+
+      this.setState({
+        users: userList,
+        loading: false,
+      });
+    })
+  }
+
+  componentWillUnmount() {
+    this.props.firebase.users().off();
+  }
+
+  render() {
+    const { users, loading } = this.state;
+    return (
+      <div>
+        <h1>Admin</h1>
+        {loading && <div>Loading ... </div>}
+
+        <UserList users={users} />
+      </div>
+    );
+  }
+}
+
+const UserList = ({ users }) => (
+  <ul>
+    {users.map(user => (
+      <li key={user.uid}>
+        <span><strong>ID : </strong> {user.uid}</span>
+        <span><strong>E-mail : </strong> {user.email}</span>
+        <span><strong>UserName : </strong> {user.username}</span>
+      </li>
+    ))}
+  </ul>
 );
 
-export default Admin;
+export default withFirebase(AdminPage);
